850fca5433aefec29a7f1bb7cca81feab6637ee7
Sasha Tatar <sasa.tatar@gmail.com>
2019-05-29 15:46:29 +0200

    Optimize for production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -41,12 +41,16 @@ var specific = {
       filename: "[name].css",
       chunkFilename: "[name].css"
     }),
-    // new WorkboxPlugin.GenerateSW({
-        //     // these options encourage the ServiceWorkers to get in there fast 
-        //     // and not allow any straggling "old" SWs to hang around
-        //     clientsClaim: true,
-        //     skipWaiting: true
-        // })
+    new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast 
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true,
+        runtimeCaching: [{
+            urlPattern: /^https/,
+            handler: "NetworkFirst"
+        }]
+    })
   ],
 
   output: {
