152c035c18946c8cf6bd9a80020f08944f33cb55
Sasha Tatar <sasa.tatar@gmail.com>
2019-05-29 14:34:49 +0200

    Enable firestore offline persistance for pwa, add runtimeCachnig config

--- app/data/db/firestore.js
+++ app/data/db/firestore.js
@@ -1,8 +1,15 @@
 import "firebase/firestore";
 import { firebase } from "./firebase";
 
+firebase.firestore().enablePersistence()
+    .catch(function(err) {
+        if (err.code == 'failed-precondition') {
+            console.warn("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
+        } else if (err.code == 'unimplemented') {
+            console.warn("The current browser does not support all of the features required to enable persistence")
+        }
+
+        throw err;
+    });
+
 export const firestore = firebase.firestore();
-
-
-
-
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -52,11 +52,16 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: paths.app + 'index.html'
         }),
+        // TODO: this should be moved to webpack.prod.js, once the feature is ready
         new WorkboxPlugin.GenerateSW({
             // these options encourage the ServiceWorkers to get in there fast 
             // and not allow any straggling "old" SWs to hang around
             clientsClaim: true,
-            skipWaiting: true
+            skipWaiting: true,
+            runtimeCaching: [{
+                urlPattern: /^http/,
+                handler: "NetworkFirst"
+            }]
         })
     ]
 };
