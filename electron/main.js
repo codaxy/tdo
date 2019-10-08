48d41b83ce990f18901258f9217f1319b4b20929
Maja <maja.mihajlovic9@gmail.com>
2019-10-08 15:51:26 +0200

    add installer

--- electron/main.js
+++ electron/main.js
@@ -8,6 +8,11 @@ const path = require("path");
 let mainWindow;
 let isQuiting;
 
+if (handleSquirrelEvent(app)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
+
 function initialize() {
     makeSingleInstance();
 
@@ -17,12 +22,15 @@ function initialize() {
             width: 800,
             height: 600,
             webPreferences: {
-                nodeIntegration: true
+                nodeIntegration: true,
+                nativeWindowOpen: true
+
             },
             icon: getIconPath("favicon64x64.png")
         });
 
         mainWindow.setMenuBarVisibility(false);
+        //mainWindow.webContents.openDevTools();
 
         // and load the index.html of the app.
         // mainWindow.loadFile('index.html')
@@ -164,3 +172,66 @@ initialize();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+function handleSquirrelEvent(application) {
+    if (process.argv.length === 1) {
+        return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function (command, args) {
+        let spawnedProcess, error;
+
+        try {
+            spawnedProcess = ChildProcess.spawn(command, args, {
+                detached: true
+            });
+        } catch (error) { }
+
+        return spawnedProcess;
+    };
+
+    const spawnUpdate = function (args) {
+        return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+
+            // Install desktop and start menu shortcuts
+            spawnUpdate(['--createShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+
+            // Remove desktop and start menu shortcuts
+            spawnUpdate(['--removeShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            application.quit();
+            return true;
+    }
+};
