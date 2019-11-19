2eda5935bea013e671c994dffbf8baf3b46a7002
vladimirk4 <55479671+vladimirk4@users.noreply.github.com>
2019-11-19 09:39:41 +0100

    tooltip

--- app/routes/Controller.js
+++ app/routes/Controller.js
@@ -34,7 +34,7 @@ export default ({ store, get, set, init }) => {
                 this.store.set("layout.mode", this.getLayoutMode());
             });
 
-            this.store.set('page.showToolTips', true);
+            this.store.set('page.showTooltips', true);
 
             auth.onAuthStateChanged(user => {
 
