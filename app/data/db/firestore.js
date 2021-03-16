f7b7501febcd38075f520920ee0376e39c17154b
Michal U <urbanekmike@gmail.com>
2021-03-16 11:27:24 +0000

    firestore make consistent with old version

--- app/data/db/firestore.js
+++ app/data/db/firestore.js
@@ -3,6 +3,17 @@ import {
 } from "./firebase";
 const firestore = firebase.firestore();
 
+firestore.enablePersistence()
+    .catch(function(err) {
+        if (err.code == 'failed-precondition') {
+            console.warn("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
+        } else if (err.code == 'unimplemented') {
+            console.warn("The current browser does not support all of the features required to enable persistence")
+        }
+
+        throw err;
+    });
+
 export {
     firestore
 };
